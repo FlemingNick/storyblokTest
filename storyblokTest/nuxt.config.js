11c5596bf83bc54b5d97f31767114594cefa07a6
Jan-Fleming Nickelsen <jan-fleming.nickelsen@hmmh.de>
2023-12-14 10:44:29 +0100

    first commit

---
+++ storyblokTest/nuxt.config.js
@@ -0,0 +1,15 @@
+export default defineNuxtConfig({
+  css: ['@/assets/css/roboto.css'],
+  modules: [
+    [
+      '@storyblok/nuxt',
+      {
+        accessToken: 'ArWQUsXjGHg5UV7iAniVrQtt',
+        apiOptions: {
+          region: '' // Set 'US" if your space is created in US region (EU default)
+        }
+      },
+    ],
+    '@nuxtjs/tailwindcss',
+  ]
+})
