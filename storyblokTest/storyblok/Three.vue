68b8a3b46bed00761835383336b58c6c08f0be45
Fleming Nickelsen <jan-fleming.nickelsen@hmmh.de>
2023-12-18 16:09:21 +0100

    add orbit controls

--- storyblokTest/storyblok/Three.vue
+++ storyblokTest/storyblok/Three.vue
@@ -16,6 +16,7 @@
     import * as THREE from 'three';
     import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
     import model from '@/assets/models/MemmertUN450.gltf'
+    import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
     defineProps({ blok: Object })
     
@@ -33,18 +34,19 @@
         const fov = 75;
         const aspect = 2; // the canvas default
         const near = 0.1;
-        const far = 5;
+        const far = 10;
         const camera = new THREE.PerspectiveCamera( fov, aspect, near, far );
         camera.position.z = 2;
+        const controls = new OrbitControls( camera, renderer.domElement );
 
         const scene = new THREE.Scene();
 
         {
 
             const color = 0xFFFFFF;
-            const intensity = 3;
+            const intensity = 7;
             const light = new THREE.DirectionalLight( color, intensity );
-            light.position.set( - 1, 2, 4 );
+            light.position.set( 300, 100, 3000 );
             scene.add( light );
 
         }
@@ -54,6 +56,8 @@
             renderer.render( scene, camera );
 
             requestAnimationFrame( render );
+
+            controls.update();
 
         }
 
