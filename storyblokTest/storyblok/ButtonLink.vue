7cbe66a888233c2abe2e4df91f38d0aa640acfcc
Karim Buchalla <karim.buchalla@hmmh.de>
2024-02-06 19:16:52 +0100

    mouse over animation

--- storyblokTest/storyblok/ButtonLink.vue
+++ storyblokTest/storyblok/ButtonLink.vue
@@ -1,20 +1,52 @@
 <template>
   <a
+    ref="buttonRef"
     v-editable="blok"
     class="btn-link"
     v-bind:href="blok.link"
   >
-    {{ blok.name }}
+    <span>
+      {{ blok.name }}
+    </span>
     <img
       src="@/assets/images/chevron.svg"
       alt="navigation arrow"
       aria-hidden
     />
+
+    <div
+      ref="buttonHoverRef"
+      class="desktop-hover"
+    ></div>
   </a>
 </template>
 
 <script setup>
+  import { ref, onMounted } from 'vue';
+  import gsap from 'gsap';
+
   defineProps({ blok: Object });
+
+  let buttonRef = ref();
+  let buttonHoverRef = ref();
+
+  onMounted(() => {
+    window.addEventListener('mousemove', (e) => {
+      let buttonBoundingClient = buttonRef.value.getBoundingClientRect();
+      let mouseOriginX = e.clientX - buttonBoundingClient.left;
+      let mouseOriginY = e.clientY - buttonBoundingClient.top;
+      let mouseDamping = 0;
+
+      console.log(mouseOriginY);
+
+      gsap.to(buttonHoverRef.value, {
+        x: mouseOriginX,
+        y: mouseOriginY,
+        duration: 0.3,
+        ease: 'none'
+      });
+    });
+  });
 </script>
 
 <style lang="scss" scoped>
