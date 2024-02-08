40d512f021a0720bfb2f1dd5cab90f9d768414d8
Karim Buchalla <karim.buchalla@hmmh.de>
2024-02-08 14:13:01 +0100

    button changes

--- storyblokTest/storyblok/Teaser.vue
+++ storyblokTest/storyblok/Teaser.vue
@@ -20,33 +20,38 @@
               <path
                 d="M0.505,1L-0,1L-0,0.489C-0,0.219 0.222,-0 0.495,0L1,0L1,0.511C1,0.781 0.778,1 0.505,1Z"
                 style="fill: white; fill-rule: nonzero"
-                :style="blok.imageRight ? 'transform: rotate(90deg)' : ''"
+                :style="blok.imageRight ? 'transform: rotate(180deg)' : 'transform: rotate(90deg)'"
               />
             </clipPath>
           </defs>
         </svg>
 
-        <img
-          v-bind:src="blok.media.filename"
-          v-bind:alt="blok.media.name"
-          :style="{ clipPath: 'url(#' + blok._uid + ')' }"
-        />
+        <div class="image-wrapper">
+          <img
+            v-bind:src="blok.media.filename"
+            v-bind:alt="blok.media.name"
+            :style="{ clipPath: 'url(#' + blok._uid + ')' }"
+          />
+        </div>
       </div>
       <div class="content-wrapper">
         <h2 class="sub-header">{{ blok.subheadline }}</h2>
         <h3>{{ blok.headline }}</h3>
         <p>{{ blok.text }}</p>
-        <a
-          href="#"
-          class="btn"
-          >Zur Health Academy</a
-        >
+
+        <StoryblokComponent
+          v-for="blok in blok.button"
+          :key="blok._uid"
+          :blok="blok"
+        />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
+  import ButtonLink from './ButtonLink.vue';
+
   defineProps({ blok: Object });
 </script>
 
